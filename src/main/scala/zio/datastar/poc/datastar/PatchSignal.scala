334e1e0e349940d235b3d19b508b71079f5f38ed
Jules Ivanic <jules.ivanic@gmail.com>
2025-07-26 14:16:04 +1000

    Fix encoding (#5)
    * Fix encoding of event
    
    * Fix encoding of event
    
    * Fix encoding of event

--- src/main/scala/zio/datastar/poc/datastar/PatchSignal.scala
+++ src/main/scala/zio/datastar/poc/datastar/PatchSignal.scala
@@ -1,17 +1,24 @@
 package zio.datastar.poc.datastar
 
-import zio.datastar.poc.extensions.Extended
 import zio.http.ServerSentEvent
-import zio.json.JsonEncoder
+import zio.http.codec.HttpContentCodec
+import zio.json.{EncoderOps, JsonEncoder}
 import zio.prelude.Subtype
+import zio.schema.Schema
 
 type PatchSignal[State] = PatchSignal.Type
-object PatchSignal extends Subtype[ServerSentEvent[String]] with Extended[ServerSentEvent[String]] {
+object PatchSignal extends Subtype[ServerSentEvent[String]] {
   def apply[State: JsonEncoder](data: State): PatchSignal[State] =
     wrap(
       ServerSentEvent(
-        data = s"signals ${JsonEncoder[State].encodeJson(data, indent = None)}",
+        data = s"signals ${data.toJson}",
         eventType = Some("datastar-patch-signals")
       )
     )
+
+  /**
+   * Needed otherwise the zio-http `Endpoint::outStream` method doesn't pick the correct `HttpContentCodec` implicit
+   * and returns a HTTP response with `Content-Type: application/json` instead of `text/event-stream`.
+   */
+  given [State]: HttpContentCodec[PatchSignal[State]] = wrapAll(ServerSentEvent.defaultContentCodec(using Schema.primitive[String]))
 }
