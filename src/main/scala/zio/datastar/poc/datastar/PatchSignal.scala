f365a1df7dab98cade7f3fa4cf61e09fe473b0c7
Jules Ivanic <jules.ivanic@gmail.com>
2025-07-26 14:35:37 +1000

    Prefer dependency-free opaque type encoding over zio-prelude newtype encoding

--- src/main/scala/zio/datastar/poc/datastar/PatchSignal.scala
+++ src/main/scala/zio/datastar/poc/datastar/PatchSignal.scala
@@ -3,22 +3,21 @@ package zio.datastar.poc.datastar
 import zio.http.ServerSentEvent
 import zio.http.codec.HttpContentCodec
 import zio.json.{EncoderOps, JsonEncoder}
-import zio.prelude.Subtype
 import zio.schema.Schema
 
-type PatchSignal[State] = PatchSignal.Type
-object PatchSignal extends Subtype[ServerSentEvent[String]] {
+opaque type PatchSignal[State] = ServerSentEvent[String]
+object PatchSignal {
+  private val eventType: Option[String] = Some("datastar-patch-signals")
+
   def apply[State: JsonEncoder](data: State): PatchSignal[State] =
-    wrap(
-      ServerSentEvent(
-        data = s"signals ${data.toJson}",
-        eventType = Some("datastar-patch-signals")
-      )
+    ServerSentEvent(
+      data = s"signals ${data.toJson}",
+      eventType = eventType
     )
 
   /**
    * Needed otherwise the zio-http `Endpoint::outStream` method doesn't pick the correct `HttpContentCodec` implicit
    * and returns a HTTP response with `Content-Type: application/json` instead of `text/event-stream`.
    */
-  given [State]: HttpContentCodec[PatchSignal[State]] = wrapAll(ServerSentEvent.defaultContentCodec(using Schema.primitive[String]))
+  given [State]: HttpContentCodec[PatchSignal[State]] = ServerSentEvent.defaultContentCodec(using Schema.primitive[String])
 }
